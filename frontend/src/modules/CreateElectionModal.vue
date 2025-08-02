bb296cbf7bee01dba5f811f729fc072c5b67e1e3
saeed abdulrauf <abdulraufsaeed@gmail.com>
2025-08-01 17:21:58 -0700

    feat(modal): close modal on backdrop click with click.stop inside modal

--- frontend/src/modules/CreateElectionModal.vue
+++ frontend/src/modules/CreateElectionModal.vue
@@ -4,6 +4,7 @@ import BaseTextArea from '@/components/BaseTextArea.vue'
 import BaseBtn from '@/components/BaseBtn.vue'
 
 import { Plus, X } from 'lucide-vue-next'
+
 const emit = defineEmits<{
   (e: 'close'): void
 }>()
@@ -12,10 +13,10 @@ const emit = defineEmits<{
 <template>
   <div
     class="h-screen w-screen bg-black/50 fixed top-0 left-0 flex items-center justify-center z-50"
+    @click="emit('close')"
   >
-    <div class="bg-white rounded-lg p-6 w-full max-w-3xl relative m-4 md:m-6">
+    <div class="bg-white rounded-lg p-6 w-full max-w-3xl relative m-4 md:m-6" @click.stop>
       <!-- Close Icon -->
-      <!-- This was missing emit -->
       <button
         class="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none"
         @click="emit('close')"
@@ -32,17 +33,6 @@ const emit = defineEmits<{
             <label class="block text-sm font-medium text-gray-700">Position Title</label>
             <BaseInput type="text" placeholder="e.g., President, Vice President" />
           </div>
-
-          <!-- Remove Button -->
-          <!-- <div>
-            <label class="block text-sm font-medium text-gray-700">Election Positions</label>
-            <BaseBtn
-              class="inline-flex items-center mt-1 gap-2 border-2 border-orange-600 text-orange-600 px-4 py-2 rounded-lg cursor-pointer"
-            >
-              <Trash2 class="h-4 w-4" />
-              Remove
-            </BaseBtn>
-          </div> -->
         </div>
 
         <!-- Description -->
@@ -50,6 +40,7 @@ const emit = defineEmits<{
           <label class="block text-sm font-medium text-gray-700">Description</label>
           <BaseTextArea placeholder="Brief description of the position..." class="mt-1" />
         </div>
+
         <!-- Button -->
         <div class="justify-end flex">
           <BaseBtn
